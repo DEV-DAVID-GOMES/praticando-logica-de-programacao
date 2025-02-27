69e25b540755d26228a5968bfb4cdbef04f9db47
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-02-27 15:22:13 -0300

    3º exercício praticando FOR

---
+++ 6º dia/3ex.js
@@ -0,0 +1,6 @@
+// 3º Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+
+for(let i = 50; i >= 0; i-= 5) {
+    console.log(i)
+}
+console.log("Fim!")
