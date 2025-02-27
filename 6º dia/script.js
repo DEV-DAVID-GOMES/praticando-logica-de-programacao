5920172c8de2d3d6dcfc02bea3356cc34a3e5bce
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-02-27 15:05:43 -0300

    1º exercicio praticando FOR

---
+++ 6º dia/script.js
@@ -0,0 +1,7 @@
+// Faça um algoritmo que receba um valor, inteiro e positivo, e usando a estrutura FOR, mostre no console uma contagem de 0 até o número recebido.
+
+let numero = parseFloat(prompt("Insira um numero inteiro:"))
+
+for(let i = 0; i <= numero; i++) {
+    console.log(i)
+}
