b36d71d92883acfc5effacdc2cd3068c5d5113a6
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-03-09 14:40:19 -0300

    praticando - Função + Array

--- 15º dia/script.js
+++ 15º dia/script.js
@@ -1,7 +1,17 @@
-let lista1 = [13,20,10,22,24,20,27,17,19,14]
-let lista2 = [24,22,23,11,15,17,29,14,16,21]
-let numero = []
+let arrayA = [13, 20, 10, 22, 24, 20, 27, 17, 19, 14];
+let arrayB = [24, 22, 23, 11, 15, 17, 29, 14, 16, 21];
 
-function numerosEmComum(num1, num2) {
+function numerosEmComum(arr1, arr2) {
+    let resultado = [];
     
+    arr1.forEach(num => {
+        if (arr2.includes(num) && !resultado.includes(num)) {
+            resultado.push(num);
+        }
+    });
+
+    return resultado;
 }
+
+let resultado = numerosEmComum(arrayA, arrayB);
+console.log(resultado);
