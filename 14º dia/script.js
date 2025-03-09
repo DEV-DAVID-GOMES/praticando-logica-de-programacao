18bce4be15225419fe24ce036263476a96b83ee8
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-03-09 13:42:06 -0300

    praticando funções

--- 14º dia/script.js
+++ 14º dia/script.js
@@ -1,10 +1,16 @@
-// 1º Crie uma função que pergunte o nome e o salário de um colaborador;
+// 1- Crie uma função que pergunte o nome e o salário de um colaborador;
 
 // 2 - Depois crie uma função que deve ser chamada dentro da função que solicita as informações, essa função irá calcular o aumento de salário conforme a seguinte tabela:
 //        - ATÉ R$1.500,00 : aumento de 20%
 //        - R$1.501,00 até R$2.000,00 : aumento de 15%
 //        - R$2.001,00 até R$3.000,00 : aumento de 10%
 //        - R$3.001,00 em diante : aumento de 5%
+// 3-Depois de calcular o aumento você deve exibir: o nome do colaborador, o salário, a % de aumento e o salário reajustado.
+
+// 4 - Crie uma função que  deve ser chamada no final da função que calcula o reajuste salarial, essa função deve perguntar se o usuário deseja calcular novamente com novas informações.
+
+// 5 - Para testar basta chamar a primeira função criada, e as outras serão chamadas na sequência conforme a execução do código
+
 
 function colaborador() {
     let nome = prompt("Insira o nome do colaborador:")
