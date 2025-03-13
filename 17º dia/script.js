f23e18fec29c64c604e63bfed13b0c6a17161721
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-03-13 14:56:22 -0300

    adicionando a função cadastrar reserva

--- 17º dia/script.js
+++ 17º dia/script.js
@@ -41,13 +41,33 @@ class Hotel {
 }
 function cadastrarHotel() {
 
-     id = parseInt(prompt("Insria o ID do hotel:"))
-     nome = prompt("Insira o nome do hotel:")
-     categoria = prompt("Insira a categoria do hotel:")
-     endereco = prompt("INsira o endereço do hotel:")
-     telefone = prompt("Insira o telefone do hotel:")
+     let id = parseInt(prompt("Insira o ID do hotel:"))
+     let nome = prompt("Insira o nome do hotel:")
+     let categoria = prompt("Insira a categoria do hotel:")
+     let endereco = prompt("Insira o endereço do hotel:")
+     let telefone = prompt("Insira o telefone do hotel:")
 
      let novoHotel = new Hotel(id, nome, categoria, endereco, telefone);
      hoteis.push(novoHotel);
 }
 
+class Reserva {
+    constructor(id, idHotel, nomeResponsavel, diaDeEntrada, diaDeSaida) {
+        this.id = id
+        this.idHotel = idHotel
+        this.nomeResponsavel = nomeResponsavel
+        this.diaDeEntrada = diaDeEntrada
+        this.diaDeSaida = diaDeSaida
+    }
+}
+
+function cadastrarReserva() {
+    let id = parseInt(prompt("Insira o ID da reserva:"))
+    let idHotel = parseInt(prompt("Insira o ID do hotel:"))
+    let nomeResponsavel = prompt("Insira o nome do responsável:")
+    let diaDeEntrada = parseInt(prompt("Insira o dia entrada:"))
+    let diaDeSaida = parseInt(prompt("Insira o dia de saída:"))
+
+    let novaReserva = new Reserva(id, idHotel, nomeResponsavel, diaDeEntrada, diaDeSaida)
+    reservas.push(novaReserva)
+}
