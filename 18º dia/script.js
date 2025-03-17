a9c7b68f43f0a090779810cb91a8ad1666a7b955
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-03-17 15:22:59 -0300

    adicionando a função de criar biblioteca

--- 18º dia/script.js
+++ 18º dia/script.js
@@ -13,24 +13,27 @@ class Livro {
 }
 
 class Biblioteca {
-    constructor(nome, endereco, telefone, acervoLivros) {
+    constructor(nome, endereco, telefone) {
         this.nome = nome;
         this.endereco = endereco;
         this.telefone = telefone;
-        this.acervoLivros = acervoLivros
+        this.acervoLivros = []
     }
 }
 
+function criarBiblioteca() {
+    const nome = prompt("Digite o nome da biblioteca:");
+    const endereco = prompt("Digite o endereço da biblioteca:");
+    const telefone = prompt("Digite o telefone da biblioteca:");
+
+    return new Biblioteca(nome, endereco, telefone);
+}
+
+const minhaBiblioteca = criarBiblioteca();
+console.log(minhaBiblioteca);
+
 while(continuar) {
     
-    let titulo  = prompt("Insira o titulo do livro:")
-    let autor = prompt("Insira o nome do autor:")
-    let editora = prompt("Insira o nome da editora:")
-    let anoPublicacao = prompt("Insira o ano de publicação:")
-
-  
-    const novoLivro = new Livro(titulo, autor, editora, anoPublicacao)
-    livros.push(novoLivro)
 
     continuar = parseFloat(prompt("deseja adicionar mais livro:\n 1- SIM\n 2- NÃO"))
     if(continuar === 2) {
@@ -39,4 +42,22 @@ while(continuar) {
     }
 }
 
-console.log(livros)
+// while(continuar) {
+    
+//     let titulo  = prompt("Insira o titulo do livro:")
+//     let autor = prompt("Insira o nome do autor:")
+//     let editora = prompt("Insira o nome da editora:")
+//     let anoPublicacao = prompt("Insira o ano de publicação:")
+
+  
+//     const novoLivro = new Livro(titulo, autor, editora, anoPublicacao)
+//     livros.push(novoLivro)
+
+//     continuar = parseFloat(prompt("deseja adicionar mais livro:\n 1- SIM\n 2- NÃO"))
+//     if(continuar === 2) {
+//         continuar = false
+
+//     }
+// }
+
+// console.log(livros)
